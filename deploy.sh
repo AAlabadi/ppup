2f5e29f77e642f623271e3ab5909f96ee0f69557
sajad <sajad.alabadi@accelins.com>
2024-12-26 06:43:04 -0600

    update tepo name

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+set -e  # Exit immediately on error
+
 echo "command - npx tsc"
 npx tsc
 
 echo "command - aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 654654376195.dkr.ecr.us-west-2.amazonaws.com"
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 654654376195.dkr.ecr.us-west-2.amazonaws.com
 
-echo "command - docker build --platform linux/amd64 --progress=plain -t scraper ."
-docker build --platform linux/amd64 --progress=plain -t scraper .
+echo "command - docker build --platform linux/amd64 --progress=plain -t puppeteer-scraper ."
+docker build --platform linux/amd64 --progress=plain -t puppeteer-scraper .
 
-echo "command - docker tag scraper:latest 654654376195.dkr.ecr.us-west-2.amazonaws.com/scraper:latest"
-docker tag scraper:latest 654654376195.dkr.ecr.us-west-2.amazonaws.com/scraper:latest
+echo "command - docker tag puppeteer-scraper:latest 654654376195.dkr.ecr.us-west-2.amazonaws.com/puppeteer-scraper:latest"
+docker tag puppeteer-scraper:latest 654654376195.dkr.ecr.us-west-2.amazonaws.com/puppeteer-scraper:latest
 
-echo "command - docker push 654654376195.dkr.ecr.us-west-2.amazonaws.com/scraper:latest"
-docker push 654654376195.dkr.ecr.us-west-2.amazonaws.com/scraper:latest
+echo "command - docker push 654654376195.dkr.ecr.us-west-2.amazonaws.com/puppeteer-scraper:latest"
+docker push 654654376195.dkr.ecr.us-west-2.amazonaws.com/puppeteer-scraper:latest
