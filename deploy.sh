007e6ae5d06d78393a8027d6ec756c9f46a6b7d8
sajad <sajad.alabadi@accelins.com>
2024-12-26 06:12:57 -0600

    adding variables

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 echo "command - npx tsc"
 npx tsc
 
-echo "command - aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <aws-account-id>.dkr.ecr.<region>.amazonaws.com"
-aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <aws-account-id>.dkr.ecr.<region>.amazonaws.com
+echo "command - aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 654654376195.dkr.ecr.us-west-2.amazonaws.com"
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 654654376195.dkr.ecr.us-west-2.amazonaws.com
 
 echo "command - docker build --platform linux/amd64 --progress=plain -t scraper ."
 docker build --platform linux/amd64 --progress=plain -t scraper .
 
-echo "command - docker tag scraper:latest <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest"
-docker tag scraper:latest <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest
+echo "command - docker tag scraper:latest 654654376195.dkr.ecr.us-west-2.amazonaws.com/scraper:latest"
+docker tag scraper:latest 654654376195.dkr.ecr.us-west-2.amazonaws.com/scraper:latest
 
-echo "command - docker push <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest"
-docker push <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest
+echo "command - docker push 654654376195.dkr.ecr.us-west-2.amazonaws.com/scraper:latest"
+docker push 654654376195.dkr.ecr.us-west-2.amazonaws.com/scraper:latest
